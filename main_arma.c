7830bbd411f335b96fd10f17682c183077b6816a
Alireza Afzal aghaei <alirezaafzalaghaei@gmail.com>
2019-03-06 17:00:06 +0330

    Update main_arma.c

--- main_arma.c
+++ main_arma.c
@@ -5,28 +5,21 @@
 #include <omp.h>
 #include <armadillo>
 
-
 #define MXY
 #define NXY
 
 using namespace std;
 using namespace arma;
 
-int
-main(int argc, char** argv)
-  {
-  
-  mat A = 10 * randi<mat>(M,N);
-  mat B = 10 * randi<mat>(M,N);
-  auto start = std::chrono::high_resolution_clock::now();
-  mat C = A*B;
-  auto finish = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> elapsed = finish - start;
+int main(int argc, char ** argv) {
+  mat A = randi < mat > (M, N);
+  mat B = randi < mat > (M, N);
+  auto start = chrono::high_resolution_clock::now();
+  mat C = A * B;
+  auto finish = chrono::high_resolution_clock::now();
+  chrono::duration < double > elapsed = finish - start;
 
-  std::cout << (int) (elapsed.count() * 1000000) << endl;
+  cout << (int)(elapsed.count() * 1000000) << endl;
 
-  
-  
-  
   return 0;
-  }
+}
